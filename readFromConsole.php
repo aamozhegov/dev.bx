3dda93b907264cb52799148103bb09d970585a20
Artem Mozhegov <amozhegov@yahoo.com>
2020-11-20 00:14:11 +0300

    Изменил методы приведения переменных

--- readFromConsole.php
+++ readFromConsole.php
@@ -8,16 +8,16 @@ function readFromConsole($message='Введите что-нибудь', $user_in
 	}
 
 	if ($user_input === 'true') {
-		$result = boolval($user_input);
+		$result = (bool)$user_input;
 	}
 	elseif (is_string($user_input)) {
-		$result = strval($user_input);
+		$result = (string)$user_input;
 	}
 	elseif (is_float($user_input)) {
-		$result = float($user_input);
+		$result = (float)$user_input;
 	}
 	elseif (is_int($user_input)) {
-		$result = intval($user_input);
+		$result = (int)$user_input;
 	}
 	echo $result;
 	return $result;
