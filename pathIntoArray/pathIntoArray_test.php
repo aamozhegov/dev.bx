3eb68036aa9bf88796236cb0efc6397f360d6155
Artem Mozhegov <amozhegov@yahoo.com>
2020-11-22 23:54:21 +0300

    Написал функцию pathIntoArray_test

---
+++ pathIntoArray/pathIntoArray_test.php
@@ -0,0 +1,12 @@
+<?php
+
+require_once('./pathIntoArray.php');
+
+function test() {
+	$result = getDirectoryStatus();
+	assertEquals("", "", "Пустой путь");
+}
+
+function assertEquals($expected, $current, $message) {
+	echo $current === $expected ? "тест пройден" : "тест не пройден";
+}
