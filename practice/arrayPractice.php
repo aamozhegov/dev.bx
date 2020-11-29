93bb843a7798346997d18d58d7a0b0f2d1bd0301
Artem Mozhegov <amozhegov@yahoo.com>
2020-11-29 18:54:49 +0300

    Написал функцию arrayPractice

---
+++ practice/arrayPractice.php
@@ -0,0 +1,39 @@
+<?php
+
+
+function arrayPractice($userInput) {
+	echo 'Введите последовательность натуральных чисел, разделённых пробелом или запятой:'.PHP_EOL;
+	$arr = array_diff((preg_split('/[\s,]+/',$userInput)), array('stop'));
+	echo count(array_keys($arr, max($arr))).PHP_EOL;
+};
+
+arrayPractice('0 b');
+
+/*
+$arr = array();
+echo 'Введите натуральное число'.PHP_EOL;
+echo 'Введите "stop" чтобы завершить'.PHP_EOL;
+
+while (True) {
+	$userInput = trim(fgets(STDIN));
+	if (($userInput == 'stop') || (count($arr) == 20)){
+		break;
+	}
+	else {
+		array_push($arr, $userInput);
+	}
+}
+
+echo count($arr);
+
+
+
+Попытка реализации с do...while
+do {
+	$userInput = fgets(STDIN);
+	if ((string)$userInput == 'stop') {
+		break;
+	}
+	array_push($arr, $userInput);
+} while (True);
+*/
