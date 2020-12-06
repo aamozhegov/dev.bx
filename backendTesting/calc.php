6215eac60ddc3dd6fb38c3b0c110fea69a9e5788
Artem Mozhegov <amozhegov@yahoo.com>
2020-12-06 16:17:09 +0300

    Добавил методы класса Calculator
    Добавил методы класса Calculator: возведение в степень и вычисление квадратного корня

---
+++ backendTesting/calc.php
@@ -0,0 +1,33 @@
+<?php
+
+class Calculator {
+	public function add(int $a, int $b): int {
+		return $a + $b;
+	}
+
+	public function subtract(int $a, int $b): int {
+		return $a - $b;
+	}
+
+	public function multiply(int $a, int $b): int {
+		return $a * $b;
+	}
+
+	public function divide(int $a, int $b): float {
+		if ($b === 0) {
+			throw new \InvalidArgumentException("Divider can't be zero");
+		}
+		return $a / $b;
+	}
+
+	public function sqr(int $a, int $b): int {
+		if ($a < 0) {
+			throw new \InvalidArgumentException("Error!");
+		}
+		return sqrt($a);
+	}
+
+	public function exponentiation(int $a, int $b): int {
+		return $a ** $b;
+	}
+}
