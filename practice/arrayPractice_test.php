b6afb47b481e852b75d594e8903d27e04a3ed48f
Artem Mozhegov <amozhegov@yahoo.com>
2020-12-13 14:01:07 +0300

    Доработал функцию-тестировщик

--- practice/arrayPractice_test.php
+++ practice/arrayPractice_test.php
@@ -1,21 +1,21 @@
 <?php
 
-require_once('./arrayPractice.php');
-
-function arrayPractice_test() {
-	$result = arrayPractice('1 2 3 4 5 6 7 8 9 18 11 18 13 18 15 16 17 18 18 18 stop');
-	assertEquals(6, $result, 'arrayPractice("1 2 3 4 5 6 7 8 9 18 11 18 13 18 15 16 17 18 18 18 stop") - ');
-
-	$result = arrayPractice('1, 2, 3, 4, 5, 6, 7, 8, 9, 18, 11, 18, 13, 18, 15, 16, 17, 18, 18, 18, stop');
-	assertEquals(7, $result, 'arrayPractice("1 2 3 4 5 6 7 8 9 18 11 18 13 18 15 16 17 18 18 18 stop") - ');
-
-	$result = arrayPractice('1');
-	assertEquals(1, $result, 'arrayPractice("1 2 3 4 5 6 7 8 9 18 11 18 13 18 15 16 17 18 18 18 stop") - ');
-
-}
+require_once('arrayPractice.php');
 
 function assertEquals($expected, $given, $message) {
-	echo $message . ($given === $expected ? 'PASSED' : 'FAILED').PHP_EOL;
+	return $message . ($given === $expected ? 'PASSED' : 'FAILED').PHP_EOL;
+};
+
+function arrayPractice_test() {
+	$result = arrayPractice($userInput = '1 2 3 4 5 6 7 8 9 18 11 18 13 18 15 16 17 18 18 18 stop');
+	assertEquals(6, $result, 'arrayPractice("1 2 3 4 5 6 7 8 9 18 11 18 13 18 15 16 17 18 18 18 stop") - ');
+
+	$result = arrayPractice($userInput = '1, 2, 18, 4, 5, 7, 7, 8, 9, 18, 11, 18, 13, 18, 15, 16, 17, 18, 18, 18, stop');
+	assertEquals(7, $result, 'arrayPractice("1 2 3 4 5 6 7 8 9 18 11 18 13 18 15 16 17 18 18 18 stop") - ');
+
+	$result = arrayPractice($userInput = '1');
+	assertEquals(1, $result, 'arrayPractice("1 2 3 4 5 6 7 8 9 18 11 18 13 18 15 16 17 18 18 18 stop") - ');
+
 };
 
 arrayPractice_test();
