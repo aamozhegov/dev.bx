9d8b03f443bf7508dfa86012d65519cd00ab383c
Artem <amozhegov@yahoo.com>
2020-12-23 00:54:13 +0300

    Начал писать функцию-тестировщик

--- pathIntoArray/pathIntoArray_test.php
+++ pathIntoArray/pathIntoArray_test.php
@@ -2,3 +2,11 @@
 
 require_once('./pathIntoArray.php');
 
+function test() {
+	$result = getDirectoryStatus();
+	assertEquals("", "", "Пустой путь");
+}
+
+function assertEquals($expected, $current, $message) {
+	echo $current === $expected ? "тест пройден" : "тест не пройден";
+}
