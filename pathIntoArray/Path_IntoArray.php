d16c7fd0cba9bf21b7bd829d6db703a01d6e6874
Artem <amozhegov@yahoo.com>
2020-12-23 01:01:55 +0300

    Дописал функцию, анализирующую путь до папки

---
+++ pathIntoArray/Path_IntoArray.php
@@ -0,0 +1,33 @@
+<?php
+
+function parse_paths_of_files($path)
+{
+	if ($currentDir = opendir($path))
+	{
+		$list = [
+			'dirs' => [],
+			'files' => []
+		];
+		while (false !== ($element = readdir($currentDir)))
+		{
+			if (in_array($element, ['.', '..']))
+			{
+				continue;
+			}
+			if (is_dir($path.$element))
+			{
+				$list['dirs'][$element]['is_readable'] = (is_readable($path.$element) == true ? 'true' : 'false');
+				$list['dirs'][$element]['is_writable'] = (is_writable($path.$element) == true ? 'true' : 'false');
+				$list['files'][$element]['filesize'] = filesize($path.$element);
+			}
+		}
+		closedir($currentDir);
+
+		return $list;
+	}
+	else
+	{
+		echo 'В директории нет содержимого или введён неверный путь';
+	}
+
+}
