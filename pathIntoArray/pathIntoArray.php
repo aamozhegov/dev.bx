c5b41087566fb5cb1a97e6cbf0288c81d226505d
Artem <amozhegov@yahoo.com>
2020-12-23 00:46:37 +0300

    Исправил ошибки в теле функции

--- pathIntoArray/pathIntoArray.php
+++ pathIntoArray/pathIntoArray.php
@@ -15,9 +15,9 @@ function parse_paths_of_files($path)
 			}
 			if (is_dir($path.$element)) {
 				$list['dirs'][$element]['is_readable'] =
-					(is_readable($path.$element) == true ? 'true' : 'false');
+					(is_readable($path.$element) == true ? 'True' : 'False');
 				$list['dirs'][$element]['is_writable'] =
-					(is_writable($path.$element) == true ? 'true' : 'false');
+					(is_writable($path.$element) == true ? 'True' : 'False');
 				$list['files'][$element]['filesize'] = filesize($path.$element);
 			}
 		}
