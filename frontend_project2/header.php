4ec4fe6bf6d1e2ac2754ccac6fd7c026f80f98dc
Artem <amozhegov@yahoo.com>
2020-12-23 01:36:40 +0300

    Файл header.php

---
+++ frontend_project2/header.php
@@ -0,0 +1,55 @@
+<?php
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="utf-8">
+	<meta name="viewport"
+		  content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+	<meta http-equiv="X-UA-Compatible" content="ie-edge">
+
+	<title>Bootstrap grid demo</title>
+	<link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.5.3/dist/css/bootstrap.min.css" integrity="sha384-TX8t27EcRE3e/ihU7zmQxVncDAy5uIKz4rEkgIXeMed4M0jlfIDPvg6uqKI2xXr2" crossorigin="anonymous">
+
+	<link rel="stylesheet" href="style.css">
+</head>
+
+<body>
+<div class="wrap">
+
+
+
+
+	<header class="container-fluid">
+		<div class="row">
+			<div class="col">
+				<div class="nav">
+					<a href="" class="nav-link">link1</a>
+					<a href="" class="nav-link">link2</a>
+					<a href="" class="nav-link">link3</a>
+					<a href="" class="nav-link">link4</a>
+				</div>
+			</div>
+
+
+			<div class="col">
+				<div class="logo text-center">TRADE</div>
+			</div>
+
+			<div class="col text-right">
+				<a href="" class="btn btn-sm btn-warning">FREE TRIAL</div>
+		</div>
+
+</div>
+
+</header>
+
+<!--region menu-->
+
+
+
+
+
+
+
