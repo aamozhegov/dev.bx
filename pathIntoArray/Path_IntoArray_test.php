cb8513998f79fd21b778435eb5ce0b03429e3047
Artem <amozhegov@yahoo.com>
2020-12-23 01:13:35 +0300

    Дописал функцию-тестировщик

--- pathIntoArray/Path_IntoArray_test.php
+++ pathIntoArray/Path_IntoArray_test.php
@@ -1,3 +1,12 @@
 <?php
 
 require_once('./Path_IntoArray_test.php');
+
+function test() {
+	$result = parse_paths_of_files();
+	assertEquals("В директории нет содержимого или введён неверный путь", $result,"В директории нет содержимого или введён неверный путь");
+
+	$result = parse_paths_of_files('абракадабра');
+	assertEquals("В директории нет содержимого или введён неверный путь", $result, "В директории нет содержимого или введён неверный путь");
+}
+
