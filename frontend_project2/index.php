0509be144c5086aa65ccfaf7ce3b4a2eb83017fe
Artem <amozhegov@yahoo.com>
2020-12-23 01:37:51 +0300

    Файл index.php

---
+++ frontend_project2/index.php
@@ -0,0 +1,28 @@
+<?php include 'header.php';
+
+
+
+<div class="banner d-flex align-items-center justify-content-center">
+
+	<div class="banner-overlay"> </div>
+	<div class="banner-content">
+		<h1 class="text-white">Value proposition</h1>
+		<p class="text-white">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.</p>
+	</div>
+</div>
+
+<div style="style="padding: 50px; width: 380px;">
+	<div class="card custom_card">
+	  <img src="images/moto.png" class="card-img-top" alt="...">
+	  <div class="card-body">
+	    <h5 class="card-title">Card title</h5>
+	    <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p> 
+	    <a href="#" class="btn btn-link">Go somewhere</a>
+						   </div>
+</div>
+</div>
+
+
+
+
+<?php include 'footer.php';
