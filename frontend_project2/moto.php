9db82f57027bde3664e2c6db737a965ebe021339
Artem <amozhegov@yahoo.com>
2020-12-24 00:34:04 +0300

    Написал moto.php

---
+++ frontend_project2/moto.php
@@ -0,0 +1,19 @@
+<div class="container-fluid">
+	<div class="row" style="width: 100%">
+		<div class="col-lg-4 col-md-4 col-sm-4">
+			<img src="images/moto1.jpg" alt="moto1" class="block">
+			<h4 class="text2">Lorem ipsum dolor sit amet, consectetur adipiscing elit</h4>
+			<p class="text-black-50 underline">Learn more</p>
+		</div>
+		<div class="col-lg-4 col-md-4 col-sm-4 ">
+			<img src="images/moto2.jpg" alt="moto2" class="block">
+			<h4 class="text2">Lorem ipsum dolor sit amet, consectetur adipiscing elit</h4>
+			<p class="text-black-50 underline">Learn more</p>
+		</div>
+		<div class="col-lg-4 col-md-4 col-sm-4">
+			<img src="images/moto3.jpg" alt="moto3" class="block">
+			<h4 class="text2">Lorem ipsum dolor sit amet, consectetur adipiscing elit</h4>
+			<p class="text-black-50 underline">Learn more</p>
+		</div>
+	</div>
+</div>
