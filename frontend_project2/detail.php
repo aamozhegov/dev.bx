d68b222cac2c20ca0f2310f16a004da30b462dbe
Artem <amozhegov@yahoo.com>
2020-12-23 21:38:54 +0300

    Файл footer.php

---
+++ frontend_project2/detail.php
@@ -0,0 +1,22 @@
+<DOCTYPE html>
+
+<html lang="en">
+	<head>
+		<meta charset="UTF-8">
+		<link rel="stylesheet" href="style.css">
+		<link rel="stylesheet" href="style.css">
+		<title>Detail</title>
+		<link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.5.3/dist/css/bootstrap.min.css" integrity="sha384-TX8t27EcRE3e/ihU7zmQxVncDAy5uIKz4rEkgIXeMed4M0jlfIDPvg6uqKI2xXr2" crossorigin="anonymous">
+
+	</head>
+
+	<body>
+	<div class="header">
+		<?php include 'header.php'; ?>
+	</div>
+		<div class="container-fluid text-left">
+			<div class></div>
+		</div>
+
+
+	</body>
--- frontend_project2/footer.php
+++ frontend_project2/footer.php
@@ -1,7 +1,14 @@
-Social
-footer
 
+<link rel="stylesheet" href="style.css">
 
-</div> <!-- end .wrap-->
-</body>
-</html>
+<footer>
+	<div class="container-fluid color-black bg-dark text-white">
+		<div class="container" style="height: 380px">
+			<div class="row justify-content-center text-center">
+				<div class="space">
+				</div>
+				<p class="logo text-left"> TRADE</p>
+			</div>
+		</div>
+	</div>
+</footer>
